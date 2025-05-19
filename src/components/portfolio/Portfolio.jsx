bc7075092e94ac9c98ef5cc35e603265585f5459
Maneesh <maneeshbugaliya@gmail.com>
2025-05-19 17:24:14 +0530

    homefix project added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,24 +3,32 @@ import "./portfolio.css";
 import IMG2 from "../../assets/snakegame.png";
 import IMG1 from "../../assets/Mass-mail.png";
 import IMG3 from "../../assets/flashcard.png";
-
+import IMG4 from "../../assets/homefix.png";
 const data = [
+ 
   {
     id: 1,
+    image: IMG4,
+    title: "HomeFix – The Smart Platform for Home Maintenance",
+    github: "https://github.com/maneesh4562/HomeFix",
+    demo: "https://home-fix-mu.vercel.app",
+  },
+  {
+    id: 3,
     image: IMG1,
     title: "This can your Bulk mail engine - MassMailer",
     github: "https://github.com/maneesh4562/mass-mail",
     demo: "https://maneesh-bulkmail.netlify.app/",
   },
   {
-    id: 2,
+    id: 4,
     image: IMG2,
     title: "Snake Game",
     github: "https://github.com/maneesh4562/-Classic-Snake-Game-Built-with-JavaScript",
     demo: "https://app.netlify.com/sites/snake-playground/overview",
   },
   {
-    id:3,
+    id:2,
     image:IMG3,
     title: "Learn Faster-Flashcard",
     github: "https://github.com/maneesh4562/flashlearn-spaced-repetition",
